d13ee2fc004fddaac22249ec7393848a80dfe7ef
sanjay0202 <sanjayguru13@gmail.com>
2023-07-08 21:30:02 +0530

    day 8 p1

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
+from .models import Listing
+
 def index(request):
-    return render(request,'listings/listings.html')
+    listings = Listing.objects.all()
+    return render(request,'listings/listings.html',listings)
 
 def listing(request):
     return render(request,'listings/listing.html')
 
 def search(request):
+
     return render(request,'listings/search.html')
